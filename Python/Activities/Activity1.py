673eaf3579aa576df0ca4800eba944bcafdb8403
ghoshSniper <135579239+ghoshSniper@users.noreply.github.com>
2023-06-19 00:39:40 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name :")
+age = input("Enter you age :")
+remaining_age = 100 - int(age)
+yearOfHundred = 2023 + remaining_age
+yearOfHundred = str(yearOfHundred)
+op = f"Hi {name} , you will turn hundred on {yearOfHundred}."
+print (op)
